2aaa04f85e57d7aae9a74e0e9d7a2880916e110e
waltergu <waltergu1989@gmail.com>
2021-09-14 11:19:44 +0800

    Now it starts.

--- src/TightBindingApproximation.jl
+++ src/TightBindingApproximation.jl
@@ -1,5 +1,16 @@
 module TightBindingApproximation
 
-# Write your package code here.
+using QuantumLattices: Lattice, Bonds, Hilbert, Table, Term, Boundary, Generator, plain
+using QuantumLattices: App, Engine, Assignment, Algorithm
 
+export TBA
+
+struct TBA{L<:Lattice, G<:Generator} <: Engine
+    lattice::L
+    H::G
 end
+@inline function TBA(lattice::Lattice, hilbert::Hilbert, table::Table, terms::Tuple{Vararg{Term}}; boundary::Boundary=plain)
+    return TBA(lattice, Generator(terms, Bonds(lattice), hilbert; half=false, table=table, boundary=boundary))
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
-using TightBindingApproximation
 using Test
+using QuantumLattices: Lattice, Point, PID, Hilbert, Fock, Table, Hopping, Onsite, Generator, Bonds, usualfockindextotuple
+using TightBindingApproximation
 
 @testset "TightBindingApproximation.jl" begin
-    # Write your tests here.
+    lattice = Lattice("Square", [Point(PID(1), (0.0, 0.0), (0.0, 0.0))],
+        vectors=[[1.0, 0.0], [0.0, 1.0]],
+        neighbors=1
+        )
+    hilbert = Hilbert(PID(1)=>Fock{:f}(1, 1, 2))
+    table = Table(hilbert, usualfockindextotuple)
+    t = Hopping(:t, 1.0, 1)
+    μ = Onsite(:μ, 0.5)
+    tba = TBA(lattice, hilbert, table, (t, μ))
+    @test tba.lattice == lattice
+    @test tba.H == Generator((t, μ), Bonds(lattice), hilbert, table=table, half=false)
 end
