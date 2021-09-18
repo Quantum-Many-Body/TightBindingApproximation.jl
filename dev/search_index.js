164e4c371073e1b4e7f013c21d524621bcfc15c8
Documenter.jl <documenter@juliadocs.github.io>
2021-09-18 03:08:25 +0000

    build based on 9031624

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TightBindingApproximation","category":"page"},{"location":"#TightBindingApproximation","page":"Home","title":"TightBindingApproximation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TightBindingApproximation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TightBindingApproximation]","category":"page"}]
+}
